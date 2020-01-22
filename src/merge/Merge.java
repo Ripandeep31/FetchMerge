e9b7dbc9937de948d69a0b6c17bf4dc7ba59022d
Ripandeep31 <59933614+Ripandeep31@users.noreply.github.com>
2020-01-22 13:57:31 -0500

    Update Merge.java

--- src/merge/Merge.java
+++ src/merge/Merge.java
@@ -17,6 +17,7 @@ public class Merge {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Fetch and Merge");
+        System.out.println("Changed remote file");
     }
     
 }
